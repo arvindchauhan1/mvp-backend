39c227e8d64f524fa6e364c51a7e643f594b0819
Arvind Chauhan <psych3080@gmail.com>
2022-04-29 18:15:08 +0530

    removed registe user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,36 +2,6 @@ const ErrorHander = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
-const crypto = require("crypto");
-const cloudinary = require("cloudinary");
-
-
-// Register a User
-exports.registerUser = catchAsyncErrors(async (req, res, next) => {
-    // const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
-    //   folder: "avatars",
-    //   width: 150,
-    //   crop: "scale",
-    // });
-
-    const { name, email, password } = req.body;
-
-    const user = await User.create({
-        name,
-        email,
-        password,
-        //   avatar: {
-        //     public_id: myCloud.public_id,
-        //     url: myCloud.secure_url,
-        //   },
-        avatar: {
-            public_id: 'sample id',
-            url: "url",
-        },
-    });
-
-    sendToken(user, 201, res);
-});
 
 // Login User
 exports.loginUser = catchAsyncErrors(async (req, res, next) => {
